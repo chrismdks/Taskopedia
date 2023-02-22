d33b026098fec2aeeb5cd3494cba9ea9374f7e8f
chrismdks <chris.mdks@gmail.com>
2023-02-22 22:04:23 +0200

    basic context added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    
-  </React.StrictMode>
+  <div>
+    <h1>REACT COURSE</h1>
+    <p>In this course, we will learn react js by building TaskOPedia!</p>
+    <ul>
+      <li>Call Ben</li>
+      <li>Go to Walmart</li>
+    </ul>
+    <p>Happy Coding!</p>
+  </div>
 );
