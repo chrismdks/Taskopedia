c5c0df97781c128bc522eb0ced691805e3979246
chrismdks <chris.mdks@gmail.com>
2023-02-22 22:13:03 +0200

    break basic context into 3 components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Header(){
+  return(
+    <h1>REACT COURSE</h1>
+  )
+}
+function Footer(){
+  return(
+    <p>Happy Coding!</p>
+  )
+}
+function MainBody(){
+  return(
+    <div>
+      <p>In this course, we will learn react js by building TaskOPedia!</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to Walmart</li>
+      </ul>
+    </div>
+  )
+}
+
 root.render(
   <div>
-    <h1>REACT COURSE</h1>
-    <p>In this course, we will learn react js by building TaskOPedia!</p>
-    <ul>
-      <li>Call Ben</li>
-      <li>Go to Walmart</li>
-    </ul>
-    <p>Happy Coding!</p>
+    <Header />
+    <MainBody />
+    <Footer />
   </div>
 );
