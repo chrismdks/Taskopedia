878c35123904be915e46ddf6e59f0c375d1ae27c
chrismdks <chris.mdks@gmail.com>
2023-02-26 12:16:45 +0200

    added CSS file under src\CSS\ folder

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import ReactDOM from 'react-dom/client';
+import "./CSS/style.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function MainHeader(){
   return(
-    <h1 className="text-primary">REACT COURSE</h1>
+    <h1 className="h1">REACT COURSE</h1>
   )
 }
 
-const subHeaderStyle={color:"blueviolet",backgroundColor:"lightgray"};
+const subHeaderStyle={color:"brown",backgroundColor:"lightgray"};
 function SubHeader(){
   return(
     <h2 style={subHeaderStyle}>This is an exciting course!</h2>
