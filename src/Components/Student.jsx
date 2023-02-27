af4a79378902c98134473931b05f3be208ea22b9
chrismdks <chris.mdks@gmail.com>
2023-02-27 11:57:26 +0200

    converted Student & StudentReview functional components to class-based components

--- src/Components/Student.jsx
+++ src/Components/Student.jsx
@@ -1,19 +1,23 @@
-const Student = (props) => {
-    return(
-        <div className='container p-2'>
-            <div className='row border'>
-                <div className='col-2'> 
-                    <img className='w-100 py-2' src={props.pic} alt="Student's Avatar"/> 
-                </div>
-                <div className='col-5'> 
-                    {props.name} <br/>
-                    Coding Experience: {props.xp} years
-                </div>
-                <div className="col-2">
-                    {props.children}
+import React from "react";
+
+class Student extends React.Component {
+    render(){
+        return(
+            <div className='container p-2'>
+                <div className='row border'>
+                    <div className='col-2'> 
+                        <img className='w-100 py-2' src={this.props.pic} alt="Student's Avatar"/> 
+                    </div>
+                    <div className='col-5'> 
+                        {this.props.name} <br/>
+                        Coding Experience: {this.props.xp} years
+                    </div>
+                    <div className="col-2">
+                        {this.props.children}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 export default Student;
--- src/Components/StudentReview.jsx
+++ src/Components/StudentReview.jsx
@@ -1,9 +1,13 @@
-const StudentReview = () => {
-    return(
-        <div className="p-1">
-            <i class="bi bi-hand-thumbs-up-fill text-success p-1" style={{cursor:"pointer"}}></i>
-            <i class="bi bi-hand-thumbs-down-fill text-danger p-1" style={{cursor:"pointer"}}></i>
-        </div>
-    )
+import React from "react";
+
+class StudentReview extends React.Component {
+    render(){
+        return(
+            <div className="p-1">
+                <i class="bi bi-hand-thumbs-up-fill text-success p-1" style={{cursor:"pointer"}}></i>
+                <i class="bi bi-hand-thumbs-down-fill text-danger p-1" style={{cursor:"pointer"}}></i>
+            </div>
+        )
+    }
 }
 export default StudentReview;
